5ce0148b2436bbc7829baf1920f31c8fcc797095
katharina jäger <jaeger.kt@gmail.com>
2022-04-26 13:05:58 +0200

    Write basics of confirmation page

---
+++ pages/bookingConfirmation.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function bookingConfirmation() {
+  return (
+    <div>
+      <Head>
+        <title>myClubs workouts</title>
+        <meta name="description" content="Find your perfect next workout" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>Thank you for booking a class with myClubs</h1>
+      </div>
+    </div>
+  );
+}
